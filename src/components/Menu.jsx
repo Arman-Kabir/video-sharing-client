b7e0708dc5912c96e61e12be818f49a901ab2c04
Arman-Kabir <armankabir8@gmail.com>
2024-01-08 19:44:33 +0600

    icons added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,28 @@
 import styled from "styled-components";
-import  armanTube  from "../img/logo.png";
+import armanTube from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
 
 const Container = styled.div`
   flex: 1;
   background-color: #202020;
   height: 100vh;
   color: white;
+  font-size: 14px;
 `;
 
 const Wrapper = styled.div`
@@ -14,7 +31,7 @@ const Wrapper = styled.div`
 
 const Logo = styled.div`
   display: flex;
-  gap:5px;
+  gap: 5px;
   align-items: center;
   font-weight: bold;
   margin-bottom: 25px;
@@ -22,6 +39,14 @@ const Logo = styled.div`
 
 const Img = styled.img`
   height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0px;
 `;
 
 const Menu = () => {
@@ -32,6 +57,80 @@ const Menu = () => {
           <Img src={armanTube}></Img>
           RmanTube
         </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+        {/* <Hr /> */}
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        {/* <Hr /> */}
+        {/* <Login> */}
+          {/* Sign in to like videos, comment, and subscribe. */}
+          {/* <Link to="signin" style={{textDecoration:"none"}}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link> */}
+        {/* </Login> */}
+        {/* <Hr /> */}
+        {/* <Title>BEST OF LAMATUBE</Title> */}
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        {/* <Hr /> */}
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+        {/* <Item onClick={() => setDarkMode(!darkMode)}>
+          <SettingsBrightnessOutlinedIcon />
+          {darkMode ? "Light" : "Dark"} Mode
+        </Item> */}
       </Wrapper>
     </Container>
   );
