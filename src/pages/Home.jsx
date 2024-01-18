75d708b0f6aa315022c0a29d4291410a71a34d6c
Arman-Kabir <armankabir8@gmail.com>
2024-01-18 17:16:42 +0600

    random videos fetched

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,32 @@
 import styled from "styled-components";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap:wrap;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
 
 const Home = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get("http://localhost:8800/api/videos/random");
+      // const res = await axios.get("videos/random");
+      console.log(res.data);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, []);
+
   return (
     <Container>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      {videos.map((video) => (
+        <Card key={video._id} />
+      ))}
     </Container>
   );
 };
